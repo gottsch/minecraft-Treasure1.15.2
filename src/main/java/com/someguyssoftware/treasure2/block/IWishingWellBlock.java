7aa7aec4ff365a972ee65e868763bbf4f130ce7b
gottsch <mark.gottschling@gmail.com>
2020-09-08 02:00:47 +0000

    added wishing well blocks

---
+++ src/main/java/com/someguyssoftware/treasure2/block/IWishingWellBlock.java
@@ -0,0 +1,10 @@
+package com.someguyssoftware.treasure2.block;
+
+/**
+ * Marker interface
+ * @author Mark Gottschling on Nov 18, 2019
+ *
+ */
+public interface IWishingWellBlock extends ITreasureBlock {
+
+}
